fcf1bfb5b1dfbb5ec6014a8753770eb67066cbc2
hosi153 <ho11ho11ho11@naver.com>
2023-08-08 19:35:29 +0900

    커밋테스트 4

--- src/BurgerQueen_Sample/Main.java
+++ src/BurgerQueen_Sample/Main.java
@@ -5,8 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
 
-        System.out.println("테스트33");
-        System.out.println();
         BurgerQueen burgerQueen = new BurgerQueen();
         burgerQueen.moveToMainMenu();
 
